6f1a47be9183d4c16a7f76f32db5f8c5d9a53f6d
Thanasak1412 <Thanasak.sr60@snru.ac.th>
2024-06-07 01:44:28 +0700

    In order traversal with stack

---
+++ in-order-traversal.js
@@ -0,0 +1,46 @@
+class TreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+function inorderTraversal(root) {
+  const result = [];
+  const stack = [];
+  let current = root;
+
+  let i = 0;
+  while (current || stack.length > 0) {
+    while (current) {
+      stack.push(current);
+
+      current = current.left;
+    }
+
+    current = stack.pop();
+
+    result.push(current.val);
+    current = current.right;
+  }
+
+  return result;
+}
+
+const root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+
+const root2 = new TreeNode(1);
+root2.left = new TreeNode(2);
+root2.left.left = new TreeNode(5);
+root2.left.right = new TreeNode(6);
+root2.right = new TreeNode(3);
+
+console.log(inorderTraversal(root));
+console.log(inorderTraversal(root2));
--- quiz-lists.md
+++ quiz-lists.md
@@ -9,7 +9,7 @@
 - [✓] 69. Sqrt(x) 38.9% Easy
 - [✓] 70. Climbing Stairs 52.9% Easy
 - [✓] 88. Merge Sorted Array 49.9% Easy
-- [] 94. Binary Tree Inorder Traversal 76.3% Easy
+- [✓] 94. Binary Tree Inorder Traversal 76.3% Easy
 - [] 101. Symmetric Tree 56.7% Easy
 - [] 104. Maximum Depth of Binary Tree 75.5% Easy
 - [] 108. Convert Sorted Array to Binary Search Tree 71.8% Easy
